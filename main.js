e3f31a2b80443428d094650f90df06396519064f
Dusknery <linnea.ryd05@gmail.com>
2025-04-28 14:45:17 +0200

    fixed shit and structured css

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+const navigation = document.getElementById('navigation');
+const popupContainer = document.getElementById('popup-container');
+const closePopup = document.getElementById('close-popup');
+
+// Show the popup when the navigation button is clicked
 navigation.addEventListener('click', () => {
-    popupContainer.style.display = 'flex'; 
-  });
-  
-  closePopup.addEventListener('click', () => {
-    popupContainer.style.display = 'none'; 
+  popupContainer.style.display = 'flex'; // Show the popup
+});
+
+// Close the popup when the close button is clicked
+closePopup.addEventListener('click', () => {
+  popupContainer.style.display = 'none'; // Hide the popup
 });
